8753ac5b421380f25fc26e8757ca48422fdde065
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-05-30 00:21:19 +0100

    still workin on search

---
+++ frontend/platforme_juridique/src/components/search.vue
@@ -0,0 +1,53 @@
+<template lang="">
+   <div
+                        class="w-full mx-auto   flex items-center  justify-center  rounded-md p-4  bg-white">
+
+                        <div class="flex gap-2 flex-col  mx-auto  md:flex-row center wid">
+                            <div class="relative flex-1">
+                                <input id="departure" value="" name="departure" type="text"
+                                    class=" peer h-10 w-full border border-1.5 rounded-md border-primary-300 text-primary-900 placeholder-transparent focus:outline-none focus:border-b-600 focus:border-2 p-3"
+                                    placeholder=" " />
+                                <label for="departure"
+                                    class="absolute left-2 px-1 -top-2.5 bg-white text-primary text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-900 peer-placeholder-shown:top-2 peer-focus:-top-2.5 peer-focus:text-blue-600 peer-focus:text-sm">avocat?
+                                    :</label>
+                                <div class="absolute right-0 top-0 mt-2 mr-2">
+
+                                </div>
+                            </div>
+                            <div class="relative self-center">
+
+                            </div>
+                            <div class="relative flex-1">
+                                <select class="border p-2 rounded w-full">
+                                    <option>Round-trip</option>
+                                    <option>Missouri</option>
+                                    <option>texas</option>
+                                </select>
+                            </div>
+                            <div></div>
+                            <div class="relative flex-1">
+                                <select class="border p-2 rounded w-full">
+                                    <option>Round-trip</option>
+                                    <option>Missouri</option>
+                                    <option>texas</option>
+                                </select>
+
+
+                            </div>
+                            <div class=" relative flex-1  ">
+                                <button
+                                    class="bg-white py-3 px-5 text-primary font-semibold rounded-lg hover:shadow-lg  transition duration-3000 cursor-pointer">Rechercher</button>
+                            </div>
+                        </div>
+
+
+                    </div>
+</template>
+<script>
+export default {
+    name: search
+}
+</script>
+<style lang="">
+    
+</style>
--- frontend/platforme_juridique/src/views/users/homeView.vue
+++ frontend/platforme_juridique/src/views/users/homeView.vue
@@ -25,49 +25,7 @@
                     <p class="text-xl text-blue-200">Nous offrons toutes les réponses à vos questions juridiques,
                         rédigées par de vrais avocats.</p>
 
-                    <div
-                        class="w-full mx-auto shadow-md  flex items-center  justify-center  rounded-md p-4 pt-10 bg-white">
-
-                        <div class="flex gap-2 flex-col  mx-auto  md:flex-row center wid">
-                            <div class="relative flex-1">
-                                <input id="departure" value="" name="departure" type="text"
-                                    class=" peer h-10 w-full border border-1.5 rounded-md border-primary-300 text-primary-900 placeholder-transparent focus:outline-none focus:border-b-600 focus:border-2 p-3"
-                                    placeholder=" " />
-                                <label for="departure"
-                                    class="absolute left-2 px-1 -top-2.5 bg-white text-primary text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-900 peer-placeholder-shown:top-2 peer-focus:-top-2.5 peer-focus:text-blue-600 peer-focus:text-sm">avocat?
-                                    :</label>
-                                <div class="absolute right-0 top-0 mt-2 mr-2">
-
-                                </div>
-                            </div>
-                            <div class="relative self-center">
-
-                            </div>
-                            <div class="relative flex-1">
-                                <select class="border p-2 rounded w-full">
-                                    <option>Round-trip</option>
-                                    <option>Missouri</option>
-                                    <option>texas</option>
-                                </select>
-                            </div>
-                            <div></div>
-                            <div class="relative flex-1">
-                                <select class="border p-2 rounded w-full">
-                                    <option>Round-trip</option>
-                                    <option>Missouri</option>
-                                    <option>texas</option>
-                                </select>
-
-
-                            </div>
-                            <div class=" relative flex-1  ">
-                                <button
-                                    class="bg-white py-3 px-5 text-primary font-semibold rounded-lg hover:shadow-lg  transition duration-3000 cursor-pointer">Rechercher</button>
-                            </div>
-                        </div>
-
-
-                    </div>
+                   <Search/>
                 </div>
 
                 <!-- <div class="relative z-0 w-full  mt-8 lg:w-1/2 ">
@@ -288,11 +246,13 @@
 import { mapActions } from "vuex";
 
 import Footer from '../../components/footer/footer.vue'
+import Search from '../../components/search.vue'
 export default {
     name: "homeView",
 
     components: {
-        Footer
+        Footer,
+        Search
     },
     methods: {
         ...mapActions(["redirectTo"]),
