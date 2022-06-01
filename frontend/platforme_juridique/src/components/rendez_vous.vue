6cba822fbf68fde29ca081d6a015b404d69f327f
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-01 16:05:59 +0100

    start step2 RDV

--- frontend/platforme_juridique/src/components/rendez_vous.vue
+++ frontend/platforme_juridique/src/components/rendez_vous.vue
@@ -97,6 +97,34 @@
                                     <td class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
                                         <button class="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-primary hover:text-white focus:outline-none">choisie</button>
                                     </td>
+                          </tr> <tr>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                        <div class="flex items-center">
+                                            <div>
+                                                <div class="text-sm leading-5 text-gray-800">#1</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td></td>
+                                    <td></td>
+                                    
+                                    <td class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
+                                        <button class="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-primary hover:text-white focus:outline-none">choisie</button>
+                                    </td>
+                          </tr> <tr>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                        <div class="flex items-center">
+                                            <div>
+                                                <div class="text-sm leading-5 text-gray-800">#1</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td></td>
+                                    <td></td>
+                                    
+                                    <td class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
+                                        <button class="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-primary hover:text-white focus:outline-none">choisie</button>
+                                    </td>
                           </tr>
                         </tbody>
                     </table>
