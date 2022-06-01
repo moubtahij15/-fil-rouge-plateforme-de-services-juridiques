bee9cc86bc0ac24b4a3be9e024c88f5b6b7dc5dd
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-01 22:57:07 +0100

    still workin on form update info user component

---
+++ frontend/platforme_juridique/src/components/profileFormUser.vue
@@ -0,0 +1,84 @@
+<template lang="">
+    <div>
+         <form @submit="updateUser">
+
+            <div class="bg-white p-3 mt-6 shadow-sm rounded-sm">
+              <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
+                <span clas="text-green-500">
+                  <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                  </svg>
+                </span>
+                <span class="tracking-wide">About</span>
+              </div>
+              <div class="text-gray-700">
+                <div class="grid md:grid-cols-2 text-sm">
+                  <div class="grid grid-cols-2  mt-4 ">
+                    <div class="px-4 py-2 font-semibold">Prenom</div>
+                    <input v-model="user.prenom" placeholder="prenom"
+                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-3  py-2"
+                      required="required" type="text" name="integration[shop_name]" id="integration_shop_name">
+                  </div>
+                  <div class="grid grid-cols-2 mt-4">
+                    <div class="px-4 py-2 font-semibold">Nom</div>
+                    <input placeholder="Nom" v-model="user.nom"
+                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-3  py-2"
+                      required="required" type="text" name="integration[shop_name]" id="integration_shop_name">
+                  </div>
+                  <div class="grid grid-cols-2 mt-4">
+                    <div class="px-4 py-2 font-semibold">email</div>
+                    <input placeholder="Email " v-model="user.email"
+                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 py-2"
+                      required="required" type="email" name="integration[shop_name]" id="integration_shop_name">
+                  </div>
+                  <div class="grid grid-cols-2 mt-4">
+                    <div class="px-4 py-2 font-semibold"> ville</div>
+                    <input placeholder="date de naissance" v-model="user.ville_id"
+                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-3  py-2"
+                      required="required" type="text" name="integration[shop_name]" id="integration_shop_name">
+                  </div>
+                  <div class="grid grid-cols-2 mt-4">
+                    <div class="px-4 py-2 font-semibold"> telephone</div>
+                    <input placeholder="old password" v-model="user.tel"
+                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-3  py-2"
+                      type="password" name="integration[shop_name]" id="integration_shop_name">
+                  </div>
+                  <div class="grid grid-cols-2 mt-4 ">
+                    <div class="px-4 py-2 font-semibold">new password</div>
+                    <input placeholder="new password" v-model="user.tel"
+                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-3  py-2"
+                      type="password" name="integration[shop_name]" id="integration_shop_name">
+                  </div>
+
+                </div>
+              </div>
+              <button type="submit"
+                class="block w-full text-blue-800 text-sm font-semibold rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4">Update
+              </button>
+            </div>
+          </form>
+    </div>
+</template>
+<script>
+export default {
+    name: "profileInfoForm",
+    data() {
+        return {
+
+            user: {
+                nom: "",
+                prenom: "",
+                ville_id: "",
+                tel: "",
+                password: "",
+                email: ""
+            },
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
--- frontend/platforme_juridique/src/views/users/profileUser.vue
+++ frontend/platforme_juridique/src/views/users/profileUser.vue
@@ -48,10 +48,11 @@
                     
                  	<div @click="choix('profile')" class="  py-3 w-full cursor-pointer md:w-1/2 lg:w-full   border-solid lg:mb-0    px-12  bg-white1 border-b hover:bg-white hover:shadow-xl border-primary transform transition duration-300  hover:-translate-y-2">
 							<div class="px-6">
-                    <img src="../../assets/adistance.svg"  class=" w-12 h-12  mx-auto text-deep-purple-accent-400 sm:w-20 sm:h-20">
+               
+                    <img src="../../assets/Profile.svg"  class=" w-12 h-12  mx-auto text-deep-purple-accent-400 sm:w-20 sm:h-20">
 						</div>
 						<div class="px-6">
-						<h3 class="m-0 text-xl font-medium leading-tight border-0 border-gray-300 lg:text-xl md:text-2xl"> Consultation Juridique</h3>
+						<h3 class="m-0 text-xl font-medium leading-tight border-0 border-gray-300 lg:text-xl md:text-2xl"> Profile</h3>
 							</div>
 					</div>   
           <div @click="choix('rdv')" class="  py-3 w-full cursor-pointer md:w-1/2 lg:w-full   border-solid lg:mb-0    px-12  bg-white1 border-b hover:bg-white hover:shadow-xl border-primary transform transition duration-300  hover:-translate-y-2">
@@ -59,7 +60,7 @@
                     <img src="../../assets/Calendarsvg.svg"  class=" w-12 h-12  mx-auto text-deep-purple-accent-400 sm:w-20 sm:h-20">
 						</div>
 						<div class="px-6">
-						<h3 class="m-0 text-xl font-medium leading-tight border-0 border-gray-300 lg:text-xl md:text-2xl"> Consultation Juridique</h3>
+						<h3 class="m-0 text-xl font-medium leading-tight border-0 border-gray-300 lg:text-xl md:text-2xl">Rendez-vous</h3>
 							</div>
 					</div>   
                     
@@ -71,12 +72,12 @@
 						<h3 class="m-0 text-xl font-medium leading-tight border-0 border-gray-300 lg:text-xl md:text-2xl"> Consultation Juridique</h3>
 							</div>
 					</div>
-					<div  @click="choix('document')" class="w-full cursor-pointer md:w-1/2 lg:w-full     border-solid lg:mb-0    px-12  bg-white1 border-b hover:bg-white hover:shadow-xl border-primary transform transition duration-300  hover:-translate-y-2">
+					<div  @click="choix('document')" class=" py-3 w-full cursor-pointer md:w-1/2 lg:w-full     border-solid lg:mb-0    px-12  bg-white1 border-b hover:bg-white hover:shadow-xl border-primary transform transition duration-300  hover:-translate-y-2">
 							<div class="px-6">
-                    <img src="../../assets/Calendarsvg.svg"  class=" w-12 h-12  mx-auto text-deep-purple-accent-400 sm:w-20 sm:h-20">
+                    <img src="../../assets/document.svg"  class=" w-12 h-12  mx-auto text-deep-purple-accent-400 sm:w-20 sm:h-20">
 						</div>
 						<div class="px-6">
-						<h3 class="m-0 text-xl font-medium leading-tight border-0 border-gray-300 lg:text-xl md:text-2xl"> Télécharger ou rédiger  un document juridique </h3>
+						<h3 class="m-0 text-xl font-medium leading-tight border-0 border-gray-300 lg:text-xl md:text-2xl">mes document</h3>
 							</div>
 					</div>
             
@@ -87,67 +88,9 @@
                 </div>
                 <!-- End of profile tab -->
                     <!-- rdv -->
-                     <form @submit="updateUser">
-
-            <div class="bg-white p-3 mt-6 shadow-sm rounded-sm">
-              <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
-                <span clas="text-green-500">
-                  <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                  </svg>
-                </span>
-                <span class="tracking-wide">About</span>
-              </div>
-              <div class="text-gray-700">
-                <div class="grid md:grid-cols-2 text-sm">
-                  <div class="grid grid-cols-2  mt-4 ">
-                    <div class="px-4 py-2 font-semibold">Prenom</div>
-                    <input v-model="user.prenom" placeholder="prenom"
-                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-3  py-2"
-                      required="required" type="text" name="integration[shop_name]" id="integration_shop_name">
-                  </div>
-                  <div class="grid grid-cols-2 mt-4">
-                    <div class="px-4 py-2 font-semibold">Nom</div>
-                    <input placeholder="Nom" v-model="user.nom"
-                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-3  py-2"
-                      required="required" type="text" name="integration[shop_name]" id="integration_shop_name">
-                  </div>
-                  <div class="grid grid-cols-2 mt-4">
-                    <div class="px-4 py-2 font-semibold">email</div>
-                    <input placeholder="Email " v-model="user.email"
-                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 py-2"
-                      required="required" type="email" name="integration[shop_name]" id="integration_shop_name">
-                  </div>
-                  <div class="grid grid-cols-2 mt-4">
-                    <div class="px-4 py-2 font-semibold"> ville</div>
-                    <input placeholder="date de naissance" v-model="user.ville_id"
-                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-3  py-2"
-                      required="required" type="text" name="integration[shop_name]" id="integration_shop_name">
-                  </div>
-                  <div class="grid grid-cols-2 mt-4">
-                    <div class="px-4 py-2 font-semibold"> telephone</div>
-                    <input placeholder="old password" v-model="user.tel"
-                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-3  py-2"
-                      type="password" name="integration[shop_name]" id="integration_shop_name">
-                  </div>
-                  <div class="grid grid-cols-2 mt-4 ">
-                    <div class="px-4 py-2 font-semibold">new password</div>
-                    <input placeholder="new password" v-model="user.tel"
-                      class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-3  py-2"
-                      type="password" name="integration[shop_name]" id="integration_shop_name">
-                  </div>
-
-                </div>
-              </div>
-              <button type="submit"
-                class="block w-full text-blue-800 text-sm font-semibold rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4">Update
-              </button>
-            </div>
-          </form>
-					<div v-if="rdv">
-											<Rendez_vous />
+                    
+					<div v-if="this.profile">
+											<ProfilFormUser />
 
 					</div>
                     <!-- <div c>
@@ -169,29 +112,21 @@
 import store from "@/store";
 import { mapActions } from "vuex";
 
-import Rendez_vous from '@/components/rendez_vous.vue'
+import ProfilFormUser from '@/components/profileFormUser.vue'
 export default {
   name: "profileUser",
   data() {
     return {
       avocat: "",
-      rdv: false,
+      profile: true,
       consultation: false,
       document: false,
       users: JSON.parse(sessionStorage.getItem('User')),
 
-      user: {
-        nom: "",
-        prenom: "",
-        ville_id: "",
-        tel: "",
-        password: "",
-        email: ""
-      },
     }
   },
   components: {
-    Rendez_vous
+    ProfilFormUser
   },
 
   methods: {
