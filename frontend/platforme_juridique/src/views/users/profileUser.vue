a98192af2903518efca4782c54f9472f2346dd16
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-11 22:26:18 +0100

    Update profileUser.vue

--- frontend/platforme_juridique/src/views/users/profileUser.vue
+++ frontend/platforme_juridique/src/views/users/profileUser.vue
@@ -1,69 +1,82 @@
 <template lang="">
-
-
-<div class="bg-gray-100">
-
+  <div class="bg-gray-100">
     <!-- End of Navbar -->
 
     <div class="container mx-auto my-5 p-5">
-        <div class="md:flex no-wrap md:-mx-2 ">
-            <!-- Left Side -->
-            <div class="w-full md:w-3/12 md:mx-2">
-                <!-- Profile Card -->
-                <div class="bg-white p-3 border-t-4 border-green-400">
-                    <div class="image overflow-hidden">
-                        <img class="h-auto w-full mx-auto"
-                            src="../../assets/profileUser.jpg"
-                            alt="">
-                    </div>
-                    <h1 class="text-gray-900 font-bold text-xl leading-8 my-1">{{avocat.nom}} {{avocat.nom}}</h1>
-                  
-                </div>
-                <!-- End of profile card -->
-                <div class="my-4"></div>
-               
-                <!-- End of friends card -->
+      <div class="md:flex no-wrap md:-mx-2">
+        <!-- Left Side -->
+        <div class="w-full md:w-3/12 md:mx-2">
+          <!-- Profile Card -->
+          <div class="bg-white p-3 border-t-4 border-green-400">
+            <div class="image overflow-hidden">
+              <img
+                class="h-auto w-full mx-auto"
+                src="../../assets/profileUser.jpg"
+                alt=""
+              />
             </div>
-            <!-- Right Side -->
-            <div class="w-full md:w-9/12 mx-2 h-64">
-                <!-- Profile tab -->
-                <!-- About Section -->
-              
-                <!-- End of about section -->
+            <h1 class="text-gray-900 font-bold text-xl leading-8 my-1">
+              {{ avocat.nom }} {{ avocat.nom }}
+            </h1>
+          </div>
+          <!-- End of profile card -->
+          <div class="my-4"></div>
 
-                <div class="my-4"></div>
+          <!-- End of friends card -->
+        </div>
+        <!-- Right Side -->
+        <div class="w-full md:w-9/12 mx-2 h-64">
+          <!-- Profile tab -->
+          <!-- About Section -->
 
-                <!-- Experience and education -->
+          <!-- End of about section -->
 
-                   
-					<!-- Navigation Bar -->
+          <div class="my-4"></div>
 
-				<div class="flex flex-wrap mt-8 bg-white pb-3 rounded-2xl border-primary border-solid">
-					<!-- <div class="grid gap-8 row-gap-8 ">
+          <!-- Experience and education -->
+
+          <!-- Navigation Bar -->
+
+          <div
+            class="flex flex-wrap mt-8 bg-white pb-3 rounded-2xl border-primary border-solid"
+          >
+            <!-- <div class="grid gap-8 row-gap-8 ">
             class="grid gap-8 row-gap-8 lg:grid-cols-4"
           </div> -->
-             <div >
-                     <ChoixProfile/>
-                        
-                    </div> 
-         
-					
-                    <!-- End of Experience and education grid -->
-                </div>
-                <!-- End of profile tab -->
-                    
-					<div v-if="($store.state.choix==1) && (this.$options.name=='profileUser')">
-											<ProfilFormUser />
+            <div>
+              <ChoixProfile />
+            </div>
 
-					</div>
-                              <!-- rdv -->
+            <!-- End of Experience and education grid -->
+          </div>
+          <!-- End of profile tab -->
 
-          	<div v-if="($store.state.choix==2) && (this.$options.name=='profileUser')">
-											<rdvUser />
+          <div
+            v-if="
+              $store.state.choix == 1 && this.$options.name == 'profileUser'
+            "
+          >
+            <ProfilFormUser />
+          </div>
+          <!-- rdv -->
 
-					</div>
-            
-                    <!-- <div c>
+          <div
+            v-if="
+              $store.state.choix == 2 && this.$options.name == 'profileUser'
+            "
+          >
+            <rdvUser />
+          </div>
+
+          <div
+            v-if="
+              $store.state.choix == 3 && this.$options.name == 'profileUser'
+            "
+          >
+            <Consultation />
+          </div>
+
+          <!-- <div c>
 											<Rendez_vous />
 
 					</div>
@@ -71,19 +84,18 @@
 											<Rendez_vous />
 
 					</div> -->
-
-            </div>
-			
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 <script>
 import store from "@/store";
 import { mapActions } from "vuex";
-import ChoixProfile from '@/components/profileComponents/choixProfile.vue';
-import rdvUser from '@/components/profileComponents/user/rendezVous.vue'
-import ProfilFormUser from '@/components/profileFormUser.vue'
+import ChoixProfile from "@/components/profileComponents/choixProfile.vue";
+import rdvUser from "@/components/profileComponents/user/rendezVous.vue";
+import Consultation from "@/components/profileComponents/user/consultation.vue";
+import ProfilFormUser from "@/components/profileFormUser.vue";
 export default {
   name: "profileUser",
   data() {
@@ -93,30 +105,31 @@ export default {
       consultation: false,
       document: false,
       // users: JSON.parse(sessionStorage.getItem('User')),
-
-    }
+    };
   },
   components: {
     ProfilFormUser,
     ChoixProfile,
-    rdvUser
+    rdvUser,
+    Consultation
   },
 
   methods: {
-    ...mapActions(["redirectTo", "getVilles", "getCategorie", "registerUser", "isLogin", "getAvocats", "getAvocatsBySearch"]),
-
-
+    ...mapActions([
+      "redirectTo",
+      "getVilles",
+      "getCategorie",
+      "registerUser",
+      "isLogin",
+      "getAvocats",
+      "getAvocatsBySearch",
+    ]),
   },
   mounted() {
     console.log();
 
-   
-      // this.avocat = JSON.parse(sessionStorage.getItem('avocatProfile'));
-      ;
+    // this.avocat = JSON.parse(sessionStorage.getItem('avocatProfile'));
   },
-
-}
+};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
