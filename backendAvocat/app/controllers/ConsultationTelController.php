de2dbd65a7b89b4dc8e71b5b821f8b867b9299ac
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-12 21:48:03 +0100

    Update ConsultationTelController.php

--- backendAvocat/app/controllers/ConsultationTelController.php
+++ backendAvocat/app/controllers/ConsultationTelController.php
@@ -1,130 +1,164 @@
-<?php 
+<?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 
 header('Access-Control-Allow-Methods: POST');
- header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-class ConsultationTelController {
-
-
-  public  function index(){
+class ConsultationTelController
+{
 
 
+  public  function index()
+  {
 
-        echo "heelooo";
-    }
-   
-      // reads rdv for single client
-      public function read(){              
-        
-        $data= json_decode(file_get_contents("php://input"));
 
-        
-  
 
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
+    echo "heelooo";
+  }
 
-            $RDV = new RDV();
+  // reads rdv for single client
+  public function read()
+  {
 
-            // Blog post query
-            $result = $RDV->read($data->reff);
-           
-            // Turn to JSON & output
-            if($result){
-              echo json_encode($result);
+    $data = json_decode(file_get_contents("php://input"));
 
-            } else {echo json_encode(
-              array('message' => ' aucun Rdv pour ce client')
-                    ); }
-            
-        }  else  echo json_encode(
-          array('message' => 'change method to POST')
-                );
 
-       
-    
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      $RDV = new RDV();
+
+      // Blog post query
+      $result = $RDV->read($data->reff);
+
+      // Turn to JSON & output
+      if ($result) {
+        echo json_encode($result);
+      } else {
+        echo json_encode(
+          array('message' => ' aucun Rdv pour ce client')
+        );
+      }
+    } else  echo json_encode(
+      array('message' => 'change method to POST')
+    );
+  }
+
+
+
+
+
+  // create RDV
+  public function create()
+  {
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      //get posted data
+      $data = json_decode(file_get_contents("php://input"));
+
+      //check if paid or not
+      $stripe = new StripeController();
+
+      if ($stripe->checkSession($data->idSession)) {
+        $consultationTel = new ConsultationTel();
+
+        if ($consultationTel->create($data)) {
+
+          echo json_encode(
+            array('message' => 'Created')
+          );
+        } else {
+          echo json_encode(
+            array('message' => 'RDV Not Created')
+          );
+        }
+        // get id  last consultation telephonique
+        $consultation = $consultationTel->readLastConsulation()['id_consultation'];
+        // add facture
+        $facture = new Facture();
+        if ($facture->create($data, $consultation)) {
+
+          // echo json_encode(
+          //   array('message' => 'facture Created')
+          // );
+        }
+      } else {
+        echo (json_encode("cette consultation n'est pas payé"));
       }
 
-
-      
-    
-
-        // create RDV
-            public function create(){
-            
-
-              if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-                      $consultationTel = new ConsultationTel();
-                    //get posted data
-                    $data= json_decode(file_get_contents("php://input"));
-                   
-                   
-                  //  print_r(json_encode($data)) ;
-                    // create rdv
-                    if($consultationTel ->create($data)){
-
-                    echo json_encode(
-                            array('message' => 'Created')
-                            );
-                }else { 
-                        echo json_encode(
-                        array('message' => 'RDV Not Created')
-                        );
-                }
-
-              } else  echo json_encode(
-                      array('message' => 'change method to Post')
-                            );
-               
-
-            }
+      //  print_r(json_encode($data)) ;
+      // create rdv
+    } else  echo json_encode(
+      array('message' => 'change method to Post')
+    );
+  }
 
 
-            public function update(){
+  // readAllconultationsEcrite
 
-              if($_SERVER["REQUEST_METHOD"] == "POST"){
+  public function readAllconultationsTel($id)
+  {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-                    $RDV = new RDV();
+      $Consultation = new ConsultationTel();
 
-                      //get RDV data
-                      $data= json_decode(file_get_contents("php://input"));
-                      // create RDV
-                      if($RDV->update($data)){
+      // Blog post query
+      $result = $Consultation->readAllconultationsTele($id);
 
-                          echo json_encode(
-                              array('message' => 'updated')
-                            );
-                      }else { 
-                          echo json_encode(
-                            array('message' => 'RDV Not updated check your id')
-                          );
-                        }
+      // Turn to JSON & output
+      if ($result) {
+        echo json_encode($result);
+      } else {
+        echo json_encode(
+          array('message' => ' aucun Rdv pour ce client')
+        );
+      }
+    } else  echo json_encode(
+      array('message' => 'change method to POST')
+    );
+  }
 
-                    }else echo json_encode(
-                      array('message' => 'change method to PUT')
-                    );
+  public function update()
+  {
 
-              } 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-              // delete RDV
-              
-              public function delete(){
-                if($_SERVER["REQUEST_METHOD"] == "POST"){
-                  $RDV = new RDV();
+      $RDV = new RDV();
 
-                  $data= json_decode(file_get_contents("php://input"));
-                    return $RDV->delete($data);
-                    
-                    
-                      }else echo json_encode(
-                        array('message' => 'change method to DELETE')
-                      );
-  
-                }
-               
+      //get RDV data
+      $data = json_decode(file_get_contents("php://input"));
+      // create RDV
+      if ($RDV->update($data)) {
 
+        echo json_encode(
+          array('message' => 'updated')
+        );
+      } else {
+        echo json_encode(
+          array('message' => 'RDV Not updated check your id')
+        );
+      }
+    } else echo json_encode(
+      array('message' => 'change method to PUT')
+    );
+  }
 
+  // delete RDV
+
+  public function delete()
+  {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $RDV = new RDV();
+
+      $data = json_decode(file_get_contents("php://input"));
+      return $RDV->delete($data);
+    } else echo json_encode(
+      array('message' => 'change method to DELETE')
+    );
+  }
 }
