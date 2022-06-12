d38e1861287880d4cb3b6d205c30bf859e6be1da
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-12 21:48:02 +0100

    Update ConsultationEcrite.php

--- backendAvocat/app/models/ConsultationEcrite.php
+++ backendAvocat/app/models/ConsultationEcrite.php
@@ -16,19 +16,29 @@ class ConsultationEcrite extends DataBase
 
     // get sing RDV
 
-    public function read_single($id_RDV)
+    public function readPrixConsultaion($data)
     {
-        $sql = "select * from rdv where id_RDV = ?";
+        $sql = "select * from consultation where id_avocat = ? and type = ?";
         $result = $this->conn->prepare($sql);
 
-        if ($result->execute([$id_RDV])) {
+        if ($result->execute([$data->id, $data->type])) {
+            return $result->fetch(PDO::FETCH_ASSOC);
+        } else return false;
+    }
+    // read consutation by id
+    public function readConsultaion($id)
+    {
+        $sql = "select * from consultation where id = ? ";
+        $result = $this->conn->prepare($sql);
+
+        if ($result->execute([$id])) {
             return $result->fetch(PDO::FETCH_ASSOC);
         } else return false;
     }
 
 
 
-    // reads rdv for single client
+    // read last consultation Ecrite
     public function  readLastConsulation()
     {
 
@@ -40,6 +50,45 @@ class ConsultationEcrite extends DataBase
         if ($result->execute()) {
 
             return $result->fetch(PDO::FETCH_ASSOC);
+        } else {
+            return false;
+        }
+    }
+    // read all consultations By client
+
+    public function  readAllconultationsEcrite($id)
+    {
+
+        $result3 = [];
+        $sql = 'SELECT a.nom,a.prenom ,ce.sujet,ce.id ,ce.date ,ce.heure,c.type,c.prix FROM `consultation_ecrite` ce join avocat a on a.id=ce.id_avocat LEFT join consultation c on c.id=ce.id_consultation where ce.id_client=?';
+        $sql1 = "SELECT * FROM `reponse` WHERE  `reponse`.`id_consultation` = ? ";
+        $result = $this->conn->prepare($sql);
+        // $id_client=md5($id_client);
+        if ($result->execute([$id])) {
+
+            $result = $result->fetchALL(PDO::FETCH_ASSOC);
+
+
+
+            foreach ($result as $response) {
+                $result1 = $this->conn->prepare($sql1);
+
+                $categorie = "";
+                if ($result1->execute([$response['id']])) {
+                    $result1 = $result1->fetchALL(PDO::FETCH_ASSOC);
+                    // echo json_encode($result1);
+                    if (count($result1) == 0) {
+
+                        array_push($response, ["etat"=>"non repondu"]);
+                    } else{
+                        array_push($response, ["etat"=>" repondu"]);
+
+                    }
+                    array_push($response, $result1);
+                    array_push($result3, $response);
+                }
+            }
+            return ($result3);
         } else {
             return false;
         }
@@ -61,30 +110,29 @@ class ConsultationEcrite extends DataBase
     // create RDV
     public function create($data)
     {
-        //create instance client and creneau
-        $client = new Client();
-        $creneau = new Creneau();
-        // check existance foreign key
-            //  clean data
+        date_default_timezone_set('Africa/casablanca');
 
-            $data->sujet = htmlspecialchars(strip_tags($data->sujet));
-            $data->id_avocat = htmlspecialchars(strip_tags($data->id_avocat));
-            $data->id_client = htmlspecialchars(strip_tags($data->id_client));
-            $data->prix = htmlspecialchars(strip_tags($data->prix));
-            $data->date = htmlspecialchars(strip_tags($data->date));
-            $data->heure = htmlspecialchars(strip_tags($data->heure));
-            $sql = " INSERT INTO `consultation_ecrite` ( `sujet`, `id_avocat`, `id_client`, `prix`, `date`, `heure`) VALUES (:sujet, :id_avocat, :id_client, :prix, :date,:heure) ";
-            $result = $this->conn->prepare($sql);
-            // Bind data
-            return  $result->execute([
-                ':sujet' => $data->sujet,
-                ':id_avocat' => $data->id_avocat,
-                ':id_client' => $data->id_client,
-                ':prix' => $data->prix,
-                ':date' => $data->date,
-                ':heure' => $data->heure
-            ]);
-       
+        $dateToday = date("Y-m-d");
+        $heureNow = date("H:i:s");
+
+        //  clean data
+
+        $data->sujet = htmlspecialchars(strip_tags($data->sujet));
+        $data->id_avocat = htmlspecialchars(strip_tags($data->id_avocat));
+        $data->id_client = htmlspecialchars(strip_tags($data->id_client));
+        $data->id_consultation = htmlspecialchars(strip_tags($data->id_consultation));
+
+        $sql = " INSERT INTO `consultation_ecrite` ( `sujet`, `id_avocat`, `id_client`, `date`, `heure`,id_consultation) VALUES (:sujet, :id_avocat, :id_client , :date,:heure,:id_consultation) ";
+        $result = $this->conn->prepare($sql);
+        // Bind data
+        return  $result->execute([
+            ':sujet' => $data->sujet,
+            ':id_avocat' => $data->id_avocat,
+            ':id_client' => $data->id_client,
+            ':date' => $dateToday,
+            ':heure' => $heureNow,
+            ':id_consultation' => $data->id_consultation
+        ]);
     }
 
     // update RDV
