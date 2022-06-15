0029377efc5d9fa4dffd65b03b9fedd7020668e3
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-15 15:36:11 +0100

    Update document.vue

--- frontend/platforme_juridique/src/components/profileComponents/avocat/document.vue
+++ frontend/platforme_juridique/src/components/profileComponents/avocat/document.vue
@@ -2,6 +2,19 @@
   <!-- component -->
   <!-- component -->
   <div class="w-full mx-auto my-4 border-b-2 pb-4">
+    <div class="flex">
+      <svg viewBox="0 0 40 40" class="w-6 h-6 fill-current">
+        <path
+          d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z"
+        ></path>
+      </svg>
+
+      <p class="mx-3 font-bold">
+        vous pouvez annuler votre rendez-vous (d'une limite 1 h avant le
+        rendez-vous).​
+      </p>
+    </div>
+
     <section class="container mx-auto p-6 font-mono">
       <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
         <div class="w-full overflow-x-auto">
@@ -11,7 +24,7 @@
                 class="text-md font-semibold tracking-wide text-centre text-gray-900 bg-white border-b uppercase"
               >
                 <th class="px-4 py-3">avocat</th>
-                <th class="px-4 py-3">catégorie</th>
+                <th class="px-4 py-3">nom du document</th>
 
                 <th class="px-4 py-3">montant</th>
                 <th class="px-4 py-3">valdé</th>
@@ -20,16 +33,13 @@
             <tbody class="bg-white1">
               <tr
                 class="text-gray-700 border-white border-b-2"
-                v-for="elem in this.consultations"
+                v-for="elem in $store.state.documentsAvocat"
               >
                 <td class="px-4 py-3 text-center">
                   <p class="font-semibold">{{ elem.nom }} {{ elem.prenom }}</p>
                 </td>
-
-                <td
-                  class="px-4 py-3 text-left text-md font-semibold text-center cursor-pointer"
-                >
-                  {{ elem.sujet }}
+                <td class="px-4 py-3 text-center">
+                  <p class="font-semibold">{{ elem.nom_document }}</p>
                 </td>
 
                 <td class="px-4 py-3 text-xs">
@@ -42,7 +52,7 @@
 
                 <td class="px-4 py-3 text-sm">
                   <button
-                    @click=""
+                    @click="submit"
                     class="flex-no-shrink px-5 py-2 text-xs shadow-sm hover:shadow-lg font-bold tracking-wider border-2 hover:bg-primary hover:text-white text-primary rounded-full transition ease-in duration-300"
                   >
                     VOIR
@@ -54,10 +64,36 @@
         </div>
       </div>
     </section>
+    <stripe-checkout
+      ref="checkoutRef"
+      :pk="publishableKey"
+      :sessionId="this.sessionId"
+    />
+    <div class="max-w-xl mt-5 md:mx-auto sm:text-center lg:max-w-xl">
+      <div class="bg-gray-100 h-screen">
+        <div class="bg-white1 p-6 md:mx-auto">
+          <svg viewBox="0 0 24 24" class="text-primary w-16 h-16 mx-auto my-6">
+            <path
+              fill="currentColor"
+              d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z"
+            ></path>
+          </svg>
+          <div class="text-center">
+            <h3
+              class="md:text-2xl text-base text-gray-900 font-semibold text-center"
+            >
+              votre document est prêt
+            </h3>
+
+            <p class="text-primary y-2">Voici le lien de telechargement :</p>
+            <p>Passez une bonne journée !</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import { response } from "express";
 import { mapActions } from "vuex";
 import store from "../../../store";
 export default {
@@ -68,6 +104,9 @@ export default {
       showModal: false,
       sjt_post: "",
       rdvUpdate: {},
+      sessionId: null,
+      publishableKey:
+        "pk_test_51L31WgJpb3Br7exnkJTm0E3Kb1qn8HZpjxZ7WRUS54kYwpIJDbIBbhYaHQbZtWognSJ4GAbFFuHewkuoCRqMV65I00XKNgiwtA",
     };
   },
   methods: {
@@ -77,12 +116,26 @@ export default {
       "getAvocats",
       "getDocummentAvocat",
     ]),
+    getSession(id) {
+      console.log(id);
+      this.stripe(id).then((response) => {
+        console.log(response.data.id);
+        this.sessionId = response.data.id;
+        this.consultationEcrit.idSession = this.sessionId;
+        sessionStorage.setItem(
+          "consultationInfo",
+          JSON.stringify(this.consultationEcrit)
+        );
+      });
+    },
   },
+
   mounted() {
     this.getDocummentAvocat(
       JSON.parse(sessionStorage.getItem("avocatProfile")).id
     ).then((reponse) => {
-      console.log(response);
+      console.log(reponse);
+      console.log(JSON.parse(sessionStorage.getItem("avocatProfile")).id);
     });
   },
 };
