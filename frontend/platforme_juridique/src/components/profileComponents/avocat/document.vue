bd2e6fc5308a3a12c1aea987c7ab966b2cd24e35
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-15 11:39:41 +0100

    Update document.vue

---
+++ frontend/platforme_juridique/src/components/profileComponents/avocat/document.vue
@@ -0,0 +1,7 @@
+<template lang="">
+  <!-- component -->
+  <!-- component -->
+  <div class="w-full mx-auto my-4 border-b-2 pb-4">azertyuio</div>
+</template>
+<script></script>
+<style lang=""></style>
