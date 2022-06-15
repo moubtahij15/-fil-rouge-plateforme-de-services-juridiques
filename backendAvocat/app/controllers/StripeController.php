63caf99afdec915f3acbf336960aced8ad9cd618
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-15 20:50:25 +0100

    Update StripeController.php

--- backendAvocat/app/controllers/StripeController.php
+++ backendAvocat/app/controllers/StripeController.php
@@ -17,11 +17,12 @@ require_once('vendor/autoload.php');
 class StripeController
 
 {
+    // get session consultation
     public function  getSession($id)
     {
-            // get consultation info
-            $consultation=new ConsultationEcrite();
-            $consultation=$consultation->readConsultaion($id);
+        // get consultation info
+        $consultation = new ConsultationEcrite();
+        $consultation = $consultation->readConsultaion($id);
         $stripe = new \Stripe\StripeClient(
             'sk_test_51L31WgJpb3Br7exniKGWe9BNIUUYADkBzM1JHpUGdmpMU3ttTFPRSAWJqdox8CZrRJ5InGRpFnt3IhqxmpVyALVA00y7ZSPryE'
         );
@@ -35,9 +36,52 @@ class StripeController
                 [
                     'price_data' => [
                         'currency' => 'mad',
-                        'unit_amount' => $consultation["prix"]*100,
+                        'unit_amount' => $consultation["prix"] * 100,
                         'product_data' => [
-                            'name' => "consultation ".$consultation["type"]
+                            'name' => "consultation " . $consultation["type"]
+                        ]
+                    ],
+
+                    'quantity' => 1,
+                ],
+            ],
+            'mode' => 'payment',
+        ]);
+        // $checkout1 = $stripe->checkout->sessions->retrieve("cs_test_a1YpyuRq3lWzDjJWvzRmgvHSPmgKoXqCvKhvsZD79v0WJeJRNEpvxoidHW");
+
+        //    $p= $stripe->paymentIntents->confirm(
+        //         'pi_1DrDcW2eZvKYlo2CHci8WlW9',
+        //         ['payment_method' => 'pm_card_visa']
+        //     );
+        //     $session = \Stripe\Checkout\Session::retrieve('pi_3L93cNJpb3Br7exn1a9Ij1lU_secret_Sdepmtzl7KlINrZ4gp9UHQvIFd');
+        //     $customer = \Stripe\Customer::retrieve($session->customer);
+        echo json_encode($checkout);
+    }
+    // get session document
+    public function  getSessionD($id)
+    {
+        // get consultation info
+        $document = new Document();
+        $document = $document->read($id);
+        // echo json_encode($document);
+        // echo json_encode($document['prix']);
+
+        $stripe = new \Stripe\StripeClient(
+            'sk_test_51L31WgJpb3Br7exniKGWe9BNIUUYADkBzM1JHpUGdmpMU3ttTFPRSAWJqdox8CZrRJ5InGRpFnt3IhqxmpVyALVA00y7ZSPryE'
+        );
+        // \Stripe\Stripe::setApiKey('sk_test_51L31WgJpb3Br7exniKGWe9BNIUUYADkBzM1JHpUGdmpMU3ttTFPRSAWJqdox8CZrRJ5InGRpFnt3IhqxmpVyALVA00y7ZSPryE');
+
+
+        $checkout = $stripe->checkout->sessions->create([
+            'success_url' => 'http://localhost:8080/avocatProfile/',
+            'cancel_url' => 'http://localhost:8080/avocatProfile/',
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => 'mad',
+                        'unit_amount' => $document[0]["prix"] * 100,
+                        'product_data' => [
+                            'name' => "consultation " . $document[0]["nom_document"]
                         ]
                     ],
 
