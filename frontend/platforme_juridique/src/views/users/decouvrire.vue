26ab8e861bd469701a736f92502c33ad4ff27c6c
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-15 22:00:59 +0100

    Create decouvrire.vue

---
+++ frontend/platforme_juridique/src/views/users/decouvrire.vue
@@ -0,0 +1,66 @@
+<template lang="">
+  <div>
+    <div class="max-w-3xl mx-8 mt-4 lg:mx-auto md:mt-16">
+      <h1
+        class="mb-8 text-3xl font-bold leading-tight tracking-tight sm:text-4xl md:text-5xl"
+      >
+        Keep your Team in the Loop.
+      </h1>
+      <h2
+        class="inline-block px-2 mb-4 text-base font-semibold leading-normal tracking-wide text-gray-800 bg-yellow-200 sm:text-lg"
+      >
+        Having trouble managing your team?
+      </h2>
+      <p
+        class="mb-8 text-base font-light leading-relaxed text-gray-700 sm:text-lg"
+      >
+        Our service can help you keep your team in the loop on latest news,
+        updates, and so much more. Start getting your team organized today.
+      </p>
+      <a
+        href="#_"
+        class="inline-flex items-center px-4 py-2 mr-1 font-bold text-white bg-blue-700 hover:bg-blue-800 rounded"
+        >Try For Free →</a
+      >
+      <a
+        href="#_"
+        class="inline-flex items-center px-4 py-2 font-bold text-gray-700 lowercase bg-gray-200 hover:bg-gray-300 hover:text-gray-900 rounded"
+        >Learn more</a
+      >
+    </div>
+    <div class="max-w-3xl mx-8 mt-4 lg:mx-auto md:mt-16">
+      <h1
+        class="mb-8 text-3xl font-bold leading-tight tracking-tight sm:text-4xl md:text-5xl"
+      >
+        Keep your Team in the Loop.
+      </h1>
+      <h2
+        class="inline-block px-2 mb-4 text-base font-semibold leading-normal tracking-wide text-gray-800 bg-yellow-200 sm:text-lg"
+      >
+        Having trouble managing your team?
+      </h2>
+      <p
+        class="mb-8 text-base font-light leading-relaxed text-gray-700 sm:text-lg"
+      >
+        Our service can help you keep your team in the loop on latest news,
+        updates, and so much more. Start getting your team organized today.
+      </p>
+      <a
+        href="#_"
+        class="inline-flex items-center px-4 py-2 mr-1 font-bold text-white bg-blue-700 hover:bg-blue-800 rounded"
+        >Try For Free →</a
+      >
+      <a
+        href="#_"
+        class="inline-flex items-center px-4 py-2 font-bold text-gray-700 lowercase bg-gray-200 hover:bg-gray-300 hover:text-gray-900 rounded"
+        >Learn more</a
+      >
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "decouvrire",
+};
+</script>
+<style lang=""></style>
