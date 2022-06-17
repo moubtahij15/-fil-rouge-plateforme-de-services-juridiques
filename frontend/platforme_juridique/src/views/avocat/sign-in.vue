b9b3411287149809b86cfeb8b91eb00ef5fbacf5
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-17 09:32:47 +0100

    Create sign-in.vue

---
+++ frontend/platforme_juridique/src/views/avocat/sign-in.vue
@@ -0,0 +1,43 @@
+<template lang="">
+  <section class="py-0 bg-white text-primary">
+    <div
+      class="flex flex-col items-center justify-between px-10 mx-auto max-w-7xl xl:px-5 lg:flex-row"
+    >
+      <div
+        class="flex flex-col items-center w-full px-10 pt-5 pb-20 lg:pt-20 lg:flex-row"
+      >
+        <div class="relative w-full max-w-md bg-cover lg:max-w-2xl lg:w-7/12">
+          <div
+            class="relative flex flex-col items-center justify-center w-full h-full lg:pr-10"
+          >
+            <img src="../../assets/Law2.svg" />
+          </div>
+        </div>
+        <signInComponent />
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import SignInComponent from "@/components/signIn.vue";
+import { mapActions } from "vuex";
+
+export default {
+  name: "sign-in",
+  components: {
+    SignInComponent,
+  },
+  methods: {
+    ...mapActions(["redirectTo", "loginUser", "isLogin"]),
+  },
+  mounted() {
+    this.isLogin().then((response) => {
+      if (response) {
+        console.log("dsq");
+        this.redirectTo({ val: "homeView" });
+      }
+    });
+  },
+};
+</script>
+<style lang=""></style>
