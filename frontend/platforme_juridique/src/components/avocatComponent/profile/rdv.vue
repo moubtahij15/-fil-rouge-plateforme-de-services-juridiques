578db10f356f8383dee960e7b1b5d64fec98cece
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-18 15:09:23 +0100

    Update rdv.vue

--- frontend/platforme_juridique/src/components/avocatComponent/profile/rdv.vue
+++ frontend/platforme_juridique/src/components/avocatComponent/profile/rdv.vue
@@ -1,7 +1,7 @@
 <template lang="">
   <!-- alert 1h before -->
   <div class="block w-full overflow-x-auto">
-    <div class="flex items-center justify-center w-full mb-12">
+    <div class="flex items-center justify-center w-full mb-5">
       <label for="toggleB" class="flex items-center cursor-pointer">
         <!-- toggle -->
         <div class="relative">
@@ -35,6 +35,66 @@
         </div>
       </label>
     </div>
+    <section class="container mx-auto p-6 font-mono">
+      <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+        <div class="w-full overflow-x-auto">
+          <table class="w-full border-white">
+            <thead>
+              <tr
+                class="text-md font-semibold tracking-wide text-centre text-gray-900 bg-white border-b uppercase"
+              >
+                <th class="px-4 py-3">Client</th>
+                <th class="px-4 py-3">sujet</th>
+                <th class="px-4 py-3">date</th>
+
+                <th class="px-4 py-3">heure</th>
+                <th class="px-4 py-3">telephone</th>
+                <th class="px-4 py-3">email</th>
+                <!-- <th class="px-4 py-3">annuler</th> -->
+              </tr>
+            </thead>
+            <tbody class="bg-white1">
+              <tr
+                class="text-gray-700 border-white border-b-2"
+                v-for="elem in rdv"
+              >
+                <td class="px-4 py-3 text-center">
+                  <p class="font-semibold">{{ elem.nom }} {{ elem.prenom }}</p>
+                </td>
+
+                <td
+                  class="px-4 py-3 text-left text-md font-semibold text-center cursor-pointer"
+                  @click="toggleModal(elem)"
+                >
+                  {{ elem.sjt_RDV.substring(0, 20) }}
+                </td>
+                <td
+                  class="px-4 py-3 text-left text-md font-semibold text-center"
+                >
+                  {{ elem.date_creneau }}
+                </td>
+
+                <td class="px-4 py-3 text-xs">
+                  <span
+                    class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"
+                  >
+                    {{ elem.heure_debut }}
+                  </span>
+                </td>
+                <td class="px-4 py-3 text-sm">
+                  <button
+                    @click="annulerRdv(elem)"
+                    class="flex-no-shrink px-5 py-2 text-xs shadow-sm hover:shadow-lg font-bold tracking-wider border-2 hover:bg-red hover:text-white text-primary rounded-full transition ease-in duration-300"
+                  >
+                    annuler
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 <script>
@@ -78,13 +138,11 @@ export default {
       } else {
         this.value = 1;
       }
-    // console.log(this.value)
+      // console.log(this.value)
       this.chageEtatRdv({
         id: JSON.parse(sessionStorage.getItem("Avocat")).id,
         value: this.value,
-      }).then((response) => {
-
-      });
+      }).then((response) => {});
 
       //   console.log(this.value);
     },
