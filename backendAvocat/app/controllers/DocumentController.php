9dd8c26e5ce1512043ed92c7681710e7dc4ad2fa
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-06-19 23:31:22 +0100

    Update DocumentController.php

--- backendAvocat/app/controllers/DocumentController.php
+++ backendAvocat/app/controllers/DocumentController.php
@@ -40,6 +40,29 @@ class DocumentController
             array('message' => 'change method to POST')
         );
     }
+    public function readByclients($id)
+    {
+        // $data = json_decode(file_get_contents("php://input"));
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            $document = new Document();
+
+            // Blog post query
+            $document = $document->readByClients($id);
+
+            // Turn to JSON & output
+            if ($document) {
+                echo json_encode($document);
+            } else {
+                echo json_encode(
+                    array('message' => ' aucun document pour ce avocat')
+                );
+            }
+        } else  echo json_encode(
+            array('message' => 'change method to POST')
+        );
+    }
     public function readByClient($id)
     {
         // $data = json_decode(file_get_contents("php://input"));
@@ -85,6 +108,61 @@ class DocumentController
                 }
             } else
                 echo (json_encode("cette consultation n'est pas payé"));
+        } else  echo json_encode(
+            array('message' => 'change method to Post')
+        );
+    }
+
+
+    // crud document
+    public function getAlldocument($id)
+    {
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $document = new Document();                  //get client data
+            $data = json_decode(file_get_contents("php://input"));
+
+            echo $document->getAlldocument($id);
+        } else  echo json_encode(
+            array('message' => 'change method to Post')
+        );
+    }
+
+    public function createDocument()
+    {
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $document = new Document();
+            //get client data
+            $data = json_decode(file_get_contents("php://input"));
+
+            echo $document->createDocument($data);
+        } else  echo json_encode(
+            array('message' => 'change method to Post')
+        );
+    }
+    // crud document
+    public function updateDocument()
+    {
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $document = new Document();                  //get client data
+            $data = json_decode(file_get_contents("php://input"));
+
+            echo $document->updateDocument($data);
+        } else  echo json_encode(
+            array('message' => 'change method to Post')
+        );
+    }
+    // crud document
+    public function deleteDocument($id)
+    {
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $document = new Document();                  //get client data
+            $data = json_decode(file_get_contents("php://input"));
+
+            echo $document->deleteDocument($id);
         } else  echo json_encode(
             array('message' => 'change method to Post')
         );
